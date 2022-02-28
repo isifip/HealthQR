9f4bad43c0a6755d28f513c5e4ffa4f1817ee37f
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-28 16:52:39 +0400

    + Preparing the resultss

---
+++ HealthQR/Models/SmartHealthCardResults.swift
@@ -0,0 +1,22 @@
+//
+//  SmartHealthCardResults.swift
+//  HealthQR
+//
+//  Created by Irakli Sokhaneishvili on 28.02.22.
+//
+
+import Foundation
+
+enum VerificationStatus: Codable {
+    case VERIFIED, PARTIALLY_VERIFIED, NOT_VERIFIED
+}
+
+class SmartHealthCardResults {
+    var varificationStatus: VerificationStatus = .NOT_VERIFIED
+    var statusText: String = Constants.VERIFICATION_STATUS_NOT_VERIFIED
+    var statusMessage: String = Constants.VERIFICATION_STATUS_MESSAGE_NOT_VERIFIED
+    var immunizationEntries: Array<Entry> = Array<Entry>()
+    var birthDate: String = ""
+    var patientName: String = ""
+    var iss: String = ""
+}

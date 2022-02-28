1c1c3a0f4bc255445ac916c3b30249105b2d0b73
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-28 20:17:54 +0400

    +

--- HealthQR/Views/HeaderView.swift
+++ HealthQR/Views/HeaderView.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class HeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet var showDateOfBirth: UIImageView!
+    
+    private func loadNib() {
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: Bundle(for: type(of: self)))
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        view.backgroundColor = .clear
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: self.topAnchor),
+            view.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
-    */
+    
+    @objc func showDateOfBirthTapped() {
+        print("Show/hide dob")
+    }
+    
+    func setup() {
+        loadNib()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showDateOfBirthTapped))
+        showDateOfBirth.addGestureRecognizer(tapGesture)
+        showDateOfBirth.isUserInteractionEnabled = true
+        
+        showDateOfBirth.image = UserDefaults.standard.bool(forKey: Constants.SETTINGS_HIDE_DATEOFBIRTH) ? UIImage(named: "eye") : UIImage(named: "eye_hide")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+  
 
 }
